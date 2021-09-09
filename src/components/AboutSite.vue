3f693148b8a230bcfa717910f4097e7cdef90a42
takapdayon <business.takap@gmail.com>
2021-09-10 00:03:50 +0900

    youtube利用規則等記載

--- src/components/AboutSite.vue
+++ src/components/AboutSite.vue
@@ -9,6 +9,9 @@
       30分毎に更新されます<br>
       ライブ中は<v-icon color="teal darken-2">mdi-alarm</v-icon>が<v-icon color="#FF0000">mdi-youtube</v-icon>になります
     </v-card-text>
+    <v-card-text class="text-md-body-1 mt-3">
+      youtube関係は、<a href="https://www.youtube.com/t/terms">youtube利用規約</a>・<a href="https://policies.google.com/privacy">Googleポリシーと規約</a>を参考にしてください<br>
+    </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn color="green darken-1" text @click="$emit('close-dialog')">
